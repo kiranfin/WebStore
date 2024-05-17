1f4ec16b094540f767ce775a21801ddcca4a0939
kiranfin <60066861+kiranfin@users.noreply.github.com>
2024-05-17 02:28:04 +0200

    Added Cards

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-const swiper = new Swiper(".swiper", {
-    direction: 'vertical',
+let swiperCards = new Swiper('.cardcontent', {
     loop: true,
-    spaceBetween: 30,
-    grabCursor: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
+    spaceBetween: 32,
+    autoplay: {
+        delay: 5000,
     },
+    loop: true,
+    slidesPerView: 3,
+
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 });
